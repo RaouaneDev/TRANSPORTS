197b65777650a3fc5ef632ababdafefc51aee3d9
Raouane <raouanedev@gmail.com>
2021-11-09 18:57:57 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '133161d9d2780892ab13f1e36badea72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b3942a63-a135-4ffb-ae1c-9a1e9593f898'
+};
